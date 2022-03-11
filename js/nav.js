541bc9f9af5f622c4b7835288ea8fa59ad072702
NickDubDrums <niko.debe@libero.it>
2022-03-11 22:33:52 +0100

    Full Responsive V1

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+/*const mobileBtn = document.getElementById('mobile-cta')
+    nav = document.querySelector('.nav');
+    mobileBtnExit = document.getElementById('mobile-exit')
+
+mobileBtn.addEventListener('click', () => {
+    nav.classList.add('.mobile-nav-togle')
+    mobileBtnExit
+})*/
